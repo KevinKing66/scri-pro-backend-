5bf0af12b4c4fac519483232eb777142983f1275
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-10 18:58:43 -0500

    Se importa awsservice

--- src/evidences/evidences.module.ts
+++ src/evidences/evidences.module.ts
@@ -3,11 +3,12 @@ import { EvidencesService } from './evidences.service';
 import { EvidencesController } from './evidences.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EvidenceSchema } from './entities/evidence.entity';
+import { AwsModule } from 'src/aws/aws.module';
 
 @Module({
   controllers: [EvidencesController],
   providers: [EvidencesService],
-  imports: [MongooseModule.forFeature([{ name: 'Evidence', schema: EvidenceSchema }])],
+  imports: [MongooseModule.forFeature([{ name: 'Evidence', schema: EvidenceSchema }]), AwsModule],
   exports: [EvidencesService],
 })
 export class EvidencesModule {}
