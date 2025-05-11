bd97af436fd9eec9cf694e3c5aca2ebf62b7640e
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-11 13:44:49 -0500

    Se quita campo

--- src/evidences/dto/create-evidence.dto.ts
+++ src/evidences/dto/create-evidence.dto.ts
@@ -3,7 +3,7 @@ import { Participant } from '../entities/evidence.entity';
 export class CreateEvidenceDto {
   key: string;
   type: string;
-  projectUuid: string;
+  project: string;
   content: string;
   creationDateTime: Date;
   description: string;
