ee2541d797fa19b712ca6e4fa3aa540ddeabdc6f
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-14 21:46:22 -0500

    Se borra comentarios, y se cambia logica de proyecto de creacion

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -32,22 +32,22 @@ export class ProjectsService {
     if (!res) {
       throw new Error('Project not found');
     }
-    return await this.addEvidencesToProject(project.code, dto.evidences);
+    return await this.addEvidencesToProject(res._id as string, dto.evidences);
   }
 
-  async addEvidencesToProject(project: string, dto: CreateEvidenceDto[]) {
+  async addEvidencesToProject(_id: string, dto: CreateEvidenceDto[]) {
     const evidences = await Promise.all(
       dto.map(async (evidence) => {
-        const url = await this.awsService.uploadBase64Image(
+        const fileInfo = await this.awsService.uploadBase64Image(
           evidence.content,
           '',
-          project,
+          _id,
         );
 
         return {
-          key: evidence.key,
+          key: fileInfo.key,
           type: evidence.type,
-          url,
+          url: fileInfo.url,
           creationDateTime: evidence.creationDateTime,
           description: evidence.description,
           participants: evidence.participants,
@@ -56,7 +56,7 @@ export class ProjectsService {
     );
 
     return this.projectModel.updateOne(
-      { uuid: project },
+      { _id: _id },
       { $push: { evidences: { $each: evidences } } },
     );
   }
@@ -139,91 +139,3 @@ export class ProjectsService {
       : { message: 'No se encontró la evidencia con la key proporcionada' };
   }
 }
-
-/*
-
-import { Injectable } from '@nestjs/common';
-import { CreateProjectDto } from './dto/create-project.dto';
-import { UpdateProjectDto } from './dto/update-project.dto';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Project } from './entities/project.entity';
-import { AwsService } from 'src/aws/aws.service';
-import { CreateEvidenceDto } from 'src/evidences/dto/create-evidence.dto';
-
-@Injectable()
-export class ProjectsService {
-  constructor(
-    @InjectModel('Project')
-    private readonly projectModel: Model<Project>,
-  ) { }
-
-  async create(createProjectDto: CreateProjectDto) {
-    const project = new this.projectModel(createProjectDto);
-    const existingProject = await this.projectModel.findOne({
-      code: createProjectDto.code,
-    });
-    // Check if the project already exists
-    if (existingProject) {
-      throw new Error('Project already exists');
-    }
-    const res = await project.save();
-    if (!res) {
-      throw new Error('Project not found');
-    }
-    return res;
-  }
-
-  async findAll() {
-    const projects = await this.projectModel.find();
-    if (!projects) {
-      throw new Error('Projects not found');
-    }
-    return projects;
-  }
-
-  async findOne(code: string) {
-    const project = await this.projectModel.findOne({ code });
-    if (!project) {
-      throw new Error('Project not found');
-    }
-    return project;
-  }
-  async update(code: string, updateProjectDto: UpdateProjectDto) {
-    const project = await this.projectModel.findOne({ code });
-    if (!project) {
-      throw new Error('Project not found');
-    }
-    const updateFields = Object.fromEntries(
-      Object.entries(updateProjectDto).filter(
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        ([_, value]) => value !== undefined,
-      ),
-    );
-    const res = await this.projectModel.updateOne(
-      { code },
-      {
-        $set: updateFields,
-      },
-    );
-    if (!res) {
-      throw new Error('Project not found');
-    }
-    return res;
-  }
-
-  async remove(code: string) {
-    const project = await this.projectModel.findOne({ code });
-    if (!project) {
-      throw new Error('Project not found');
-    }
-    const res = await this.projectModel.deleteOne({ code });
-    if (!res) {
-      throw new Error('Project not found');
-    }
-    return res;
-  }
-}
-
-
-*/
