4a64fd734d0623dcc06fc389c2c008abaddca95e
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-17 11:24:00 -0500

    se agg manejo de errores

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import {
+  BadRequestException,
   Injectable,
+  InternalServerErrorException,
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -21,20 +23,48 @@ export class AuthService {
   ) {}
 
   async create(createUserDto: RegisterDto) {
-    const existingUser = await this.userModel.findOne({
-      email: createUserDto.email,
-    });
-    if (existingUser) {
-      throw new NotFoundException('User already exists');
+    try {
+      let existingUser = await this.userModel.findOne({
+        email: createUserDto.email,
+      });
+      if (existingUser) {
+        throw new BadRequestException('Ya existe un usuario con este email');
+      }
+
+      existingUser = await this.userModel.findOne({
+        code: createUserDto.code,
+      });
+      if (existingUser) {
+        throw new BadRequestException('Ya existe un usuario con este código');
+      }
+
+      const { password } = createUserDto;
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = new this.userModel({
+        ...createUserDto,
+        password: hashedPassword,
+      });
+
+      const res = await user.save();
+
+      if (!res) {
+        throw new InternalServerErrorException('Falló la creación del usuario');
+      }
+
+      return res;
+    } catch (error) {
+      if (
+        error instanceof BadRequestException ||
+        error instanceof InternalServerErrorException
+      ) {
+        throw error;
+      }
+
+      throw new InternalServerErrorException(
+        'Hubo un error intentando crear el usuario',
+      );
     }
-    const { password } = createUserDto;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new this.userModel({
-      ...createUserDto,
-      password: hashedPassword,
-    });
-    const res = await user.save();
-    return res;
   }
 
   async ChangePassword(data: ChangePassword) {
