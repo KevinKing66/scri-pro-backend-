d28d482fac892f8fbfbee15578c316b14caa286b
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-18 18:54:48 -0500

    Se quitan campos que no se deberian cambiar

--- src/projects/dto/update-project.dto.ts
+++ src/projects/dto/update-project.dto.ts
@@ -2,14 +2,15 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateProjectDto } from './create-project.dto';
 import { CreateFileInfoDTO } from 'src/shared/dto/create-file.dto';
 import { CreateEvidenceDto } from 'src/evidences/dto/create-evidence.dto';
+import { MemberDTO } from 'src/shared/dto/member.dto';
 
 export class UpdateProjectDto extends PartialType(CreateProjectDto) {
   code?: string | undefined;
   name?: string | undefined;
-  image?: CreateFileInfoDTO;
-  evidences?: CreateEvidenceDto[];
+  image?: CreateFileInfoDTO | undefined;
+  evidences?: CreateEvidenceDto[] | undefined;
+  members?: MemberDTO[] | undefined;
   description?: string | undefined;
   status?: 'ACTIVE' | 'COMPLETED' | 'PAUSED' | undefined;
   researchGroups?: { code: string; name: string } | undefined;
-  endDate?: Date | undefined;
 }
