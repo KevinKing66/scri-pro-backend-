47fe2d31e0b4156c3d9d42383a5ae8dc98c0cdc5
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-18 18:32:36 -0500

    Se aumenta el tamaño maximo de los request

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  app.use(bodyParser.json({ limit: '50mb' }));
 
   await app.listen(process.env.PORT ?? 3000);
 }
