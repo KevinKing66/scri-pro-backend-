04a5bb8eeb7426edfe11ea5abe17a75095b2b8fa
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-18 14:01:49 -0500

    Se agg campo de evidencias para poder actualizarlas

--- src/projects/dto/update-project.dto.ts
+++ src/projects/dto/update-project.dto.ts
@@ -1,11 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProjectDto } from './create-project.dto';
 import { CreateFileInfoDTO } from 'src/shared/dto/create-file.dto';
+import { CreateEvidenceDto } from 'src/evidences/dto/create-evidence.dto';
 
 export class UpdateProjectDto extends PartialType(CreateProjectDto) {
   code?: string | undefined;
   name?: string | undefined;
   image?: CreateFileInfoDTO;
+  evidences?: CreateEvidenceDto[];
   description?: string | undefined;
   status?: 'ACTIVE' | 'COMPLETED' | 'PAUSED' | undefined;
   researchGroups?: { code: string; name: string } | undefined;
