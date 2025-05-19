4d86a88db0fe04460e4463125248c3172d9f4659
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-18 23:19:26 -0500

    crud semillero

--- src/research-groups/dto/create-research-group.dto.ts
+++ src/research-groups/dto/create-research-group.dto.ts
@@ -2,6 +2,8 @@ export class CreateResearchGroupDto {
   code: string;
   name: string;
   description: string;
-  admin?: string;
+  admin?: { email: string; name: string };
+  faculty?: string;
+  contactEmail?: string;
   status: 'ACTIVE' | 'DESACTIVE';
 }
--- src/research-groups/entities/research-group.entity.ts
+++ src/research-groups/entities/research-group.entity.ts
@@ -4,11 +4,10 @@ export class ResearchGroup {
   code: string;
   name: string;
   description: string;
-  admin?: string;
+  admin?: { email: string; name: string };
   faculty?: string;
   knowledgeArea?: string;
   contactEmail?: string;
-  contactPhone?: string;
   status: 'ACTIVE' | 'DESACTIVE' = 'ACTIVE';
   createdAt: Date = new Date();
   updatedAt: Date = new Date();
@@ -18,11 +17,16 @@ export const ResearchGroupSchema = new mongoose.Schema({
   code: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   description: { type: String, required: true },
-  admin: { type: String, required: false },
+  admin: {
+    type: {
+      email: { type: String, required: true },
+      name: { type: String, required: false },
+    },
+    required: true,
+  },
   faculty: { type: String, required: false },
   knowledgeArea: { type: String, required: false },
   contactEmail: { type: String, required: false },
-  contactPhone: { type: String, required: false },
   status: { type: String, enum: ['ACTIVE', 'DESACTIVE'], default: 'ACTIVE' },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
