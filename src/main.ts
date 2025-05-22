7945538c1102823e141213da2e59daf479aa1c68
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-21 21:48:28 -0500

    Solucionando bugs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Load environment variables from .env file
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
